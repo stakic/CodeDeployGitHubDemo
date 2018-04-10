6d695f30f29dfdc3f02e2989631d87738abbe787
stakic <38153616+stakic@users.noreply.github.com>
2018-04-10 12:06:01 +0200

    contact

---
+++ nada/mailer.php
@@ -0,0 +1,15 @@
+<?php
+     if(isset($_POST['submit'])) {
+         $myemail = 'stakic@github.com';
+         $subject = $_POST['subject'];
+         $name = $_POST['name'];
+         $email = $_POST['email'];
+         $message = $_POST['message'];
+         $headers = 'From: Contact Form <$myemail>\r\n';
+         $headers .= 'Reply-To: $name <$email>\r\n';
+         echo 'Your message has been sent successfully!';
+         mail($myemail, $subject, $message, $headers);
+     } else {
+         echo 'An error has occurred!';
+     }
+ ?>
